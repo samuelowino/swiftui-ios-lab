b2d987adc086270e121949a7e7cede81fe9f2a2e
samuelowino <samuelowino43@yahoo.com>
2021-11-29 19:26:21 +0300

    Launch chapter two project

---
+++ chapter-two/chapter-two/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  chapter-two
+//
+//  Created by Samuel Owino on 29/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ chapter-two/chapter-two/chapter_twoApp.swift
@@ -0,0 +1,17 @@
+//
+//  chapter_twoApp.swift
+//  chapter-two
+//
+//  Created by Samuel Owino on 29/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct chapter_twoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
