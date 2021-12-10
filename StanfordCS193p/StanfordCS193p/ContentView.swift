a1640e9ccb66332c0e0a495bfc5e6c88bead3785
samuelowino <samuelowino43@yahoo.com>
2021-12-10 18:10:12 +0300

    init StanfordCS193P Lecture Series Project

---
+++ StanfordCS193p/StanfordCS193p/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StanfordCS193p
+//
+//  Created by Samuel Owino on 10/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Stanford CS193P")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StanfordCS193p/StanfordCS193p/StanfordCS193pApp.swift
@@ -0,0 +1,17 @@
+//
+//  StanfordCS193pApp.swift
+//  StanfordCS193p
+//
+//  Created by Samuel Owino on 10/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct StanfordCS193pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
